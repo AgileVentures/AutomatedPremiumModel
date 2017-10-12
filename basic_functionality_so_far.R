fd175e8636cba4a4ff2b25fd4350d1e4a0579587
Sam Joseph <tansaku@gmail.com>
2017-10-12 16:03:24 +0100

    adjusted README, added slackr and stuff

--- basic_functionality_so_far.R
+++ basic_functionality_so_far.R
@@ -1,8 +1,21 @@
-test_token <- "insert token here"
+test_token <- Sys.getenv('PRODUCTION_SLACK_AUTH_TOKEN')
 
 source("utilities.R")
+library(slackr)
 
-earliest_date = as.Date("2017-06-01")
+
+earliest_date = as.Date("2017-10-06")
 latest_date = as.Date("2017-10-07")
 
-results <- fetch_history_from_slack_channel_over_period("C0285CSUF", earliest_date, latest_date)
+
+channels <- slackr_channels(test_token)
+
+channel_histories = lapply(channels$id, function(id){
+  fetch_history_from_slack_channel_over_period(id, earliest_date, latest_date)
+})
+
+# results <- fetch_history_from_slack_channel_over_period("C0285CSUF", earliest_date, latest_date)
+
+
+
+print(head(channel_histories))

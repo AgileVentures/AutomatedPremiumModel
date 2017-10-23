68901a396e6a5343789c3f0120fb64954732c2ec
mtc2013 <mtc2013opensource@gmail.com>
2017-10-23 13:20:23 -0400

    Making travis install from special script due to reasons

---
+++ install_travis.R
@@ -0,0 +1,4 @@
+install.packages("httr", repos = "http://cran.us.r-project.org")
+install.packages("httptest", repos = "http://cran.us.r-project.org")
+install.packages("jsonlite", repos = "http://cran.us.r-project.org")
+install.packages("anytime", repos = "http://cran.us.r-project.org")

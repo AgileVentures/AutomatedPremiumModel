2d01e2e8aa5d41427c43dd2823b3fc884ab21f77
mtc2013 <mtc2013opensource@gmail.com>
2017-10-24 07:17:35 -0400

    Remove sys getenv sets in test script

--- run_tests.R
+++ run_tests.R
@@ -1,8 +1,6 @@
 options(warn=-1)
 library(httptest)
 library(devtools)
-Sys.setenv(PRODUCTION_SLACK_AUTH_TOKEN="xyz-s")
-Sys.setenv(PRODUCTION_SLACK_BOT_TOKEN="abc-d")
 devtools::install('automatedpremium')
 
 test_dir("automatedpremium/tests")

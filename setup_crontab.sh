824fc109c3236bc90fbf098ac01bb6663aedf6fd
mtc2013 <mtc2013opensource@gmail.com>
2018-03-21 12:56:26 -0400

    First attempt to filter out active premiums using WSO api and cronjob run prior to model

--- setup_crontab.sh
+++ setup_crontab.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 SHELLFILE=${HOME}/AutomatedPremiumModel/run_model.sh
+UPDATESHELLFILE = ${HOME}/AutomatedPremiumModel/run_update_of_active_premiums.sh
 cat <<EOF > crontabsetup.txt
 05 04 * * 0 ${SHELLFILE}
+05 01 * * 0 ${UPDATESHELLFILE}
 EOF
 crontab crontabsetup.txt
-
---
+++ update_active_premium_members.R
@@ -0,0 +1,21 @@
+library(httr)
+
+endpoint = "https://www.agileventures.org/api/subscriptions.json"
+token <- Sys.getenv('WSO_TOKEN')
+auth_value <- paste("Token token=\"", token,"\"",sep="")
+resp <- GET(endpoint,add_headers(authorization = auth_value))
+premium_users = jsonlite::fromJSON(content(resp, "text"))
+
+slack_token <- Sys.getenv('PRODUCTION_SLACK_AUTH_TOKEN')
+
+library(slackr)
+
+users <- slackr_users(slack_token)
+premium_users_in_slack <- merge(premium_users,users, by="email")
+premium_users_in_slack$ended_on <- as.Date(premium_users_in_slack$ended_on)
+today <- as.Date(Sys.time())
+active_premium_users_in_slack <- subset(premium_users_in_slack, (ended_on > today | is.na(ended_on)) & plan_name != "Associate")
+active_premium_users_slack_names <- unique(active_premium_users_in_slack$name)
+active_premium_users_slack_names <- c(active_premium_users_slack_names, "tansaku")
+slack_names <- data.frame(Slack=active_premium_users_slack_names)
+write.csv(slack_names, file="av_members.csv")

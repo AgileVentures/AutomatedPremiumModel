2913effd8157c106ee92af283d7f1dd2be0ede11
mtc2013 <mtc2013opensource@gmail.com>
2018-03-21 18:15:48 -0400

    Fix spacing

--- setup_crontab.sh
+++ setup_crontab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SHELLFILE=${HOME}/AutomatedPremiumModel/run_model.sh
-UPDATESHELLFILE = ${HOME}/AutomatedPremiumModel/run_update_of_active_premiums.sh
+UPDATESHELLFILE=${HOME}/AutomatedPremiumModel/run_update_of_active_premiums.sh
 cat <<EOF > crontabsetup.txt
 05 04 * * 0 ${SHELLFILE}
 05 01 * * 0 ${UPDATESHELLFILE}

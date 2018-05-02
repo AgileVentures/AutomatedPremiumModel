5f642a97da7eb61aa2f25d1a8eaec70a3c8b5dfc
marianmosley <marian.mosley@gmail.com>
2018-05-01 20:54:20 -0400

    Change name of app in shell script

--- setup_crontab.sh
+++ setup_crontab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cat <<EOF > crontabsetup.txt
 05 04 * * 0 dokku --rm run apm-production-docker Rscript basic_functionality_so_far.R
-05 01 * * 0 dokku --rm run automatedpremium-production Rscript updated_active_premium_members.R
+05 01 * * 0 dokku --rm run apm-production-docker Rscript updated_active_premium_members.R
 EOF
 sudo crontab -u dokku crontabsetup.txt
